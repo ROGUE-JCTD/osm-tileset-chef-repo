3fb807737539e3e20b9e9da2f0128af5357caa06
Moore <s30245@1630557b-s30245.mitretek.org>
2015-05-12 14:10:06 -0400

    modified:   scripts/install_postgis.sh

--- scripts/install_postgis.sh
+++ scripts/install_postgis.sh
@@ -3,8 +3,6 @@
 set -e
 
 # install Postgresql packages.  These include packages necessary for mapnik.
-#apt-get install -y postgresql-client-9.3 postgresql-9.3-postgis-2.1 postgresql-contrib-9.3 postgresql-server-dev-9.3 libpq-dev
+#apt-get install -y postgresql-client-9.3 
 apt-get install -y libboost-filesystem-dev libboost-program-options-dev libboost-python-dev libboost-regex-dev libboost-system-dev libboost-thread-dev
-apt-get install -y libicu-dev python-dev libxml2 libxml2-dev libfreetype6 libfreetype6-dev libjpeg-dev libpng-dev libproj-dev libtiff-dev libcairo2 libcairo2-dev python-cairo python-cairo-dev libcairomm-1.0-1 libcairomm-1.0-dev ttf-unifont ttf-dejavu ttf-dejavu-core ttf-dejavu-extra git build-essential python-nose libgdal1-dev python-gdal postgresql-9.1 postgresql-server-dev-9.1 postgresql-contrib-9.1 postgresql-9.1-postgis libsqlite3-dev
-apt-get install -y postgresql-9.3 postgresql-server-dev-9.3 postgresql-contrib-9.3 postgresql-9.3-postgis-2.1 php5
-
+apt-get install -y libicu-dev python-dev libxml2 libxml2-dev libfreetype6 libfreetype6-dev libjpeg-dev libpng-dev libproj-dev libtiff-dev libcairo2 libcairo2-dev python-cairo python-cairo-dev libcairomm-1.0-1 libcairomm-1.0-dev ttf-unifont ttf-dejavu ttf-dejavu-core ttf-dejavu-extra git build-essential python-nose libgdal1-dev python-gdal postgresql-9.3 postgresql-server-dev-9.3 postgresql-contrib-9.3 postgresql-9.3-postgis-2.1 libpq-dev php5 libsqlite3-dev
