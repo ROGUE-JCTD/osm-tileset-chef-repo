ecadec6e9149e2207133b9cf3d3dc2a8b17860f5
Moore <s30245@1630557b-s30245.mitretek.org>
2015-05-14 10:55:36 -0400

    modified:   Vagrantfile 	new file:   Vagrantfile.9.3 	modified:   scripts/install_packages.sh 	new file:   scripts/install_packages.sh.9.3 	renamed:    scripts/install_postgis.sh -> scripts/install_postgis.sh.9.3

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -3,21 +3,31 @@
 set -e
 
 # Update the package list
-#apt-get update
+apt-get update
 
 # install Postgresql packages.  These include packages necessary for mapnik.
 
-#apt-get install -y postgresql-client-9.3 
-apt-get install -y libboost-filesystem-dev libboost-program-options-dev libboost-python-dev libboost-regex-dev libboost-system-dev libboost-thread-dev
-apt-get install -y libicu-dev python-dev libxml2 libxml2-dev libfreetype6 libfreetype6-dev libjpeg-dev libpng-dev libproj-dev libtiff-dev libcairo2 libcairo2-dev python-cairo python-cairo-dev libcairomm-1.0-1 libcairomm-1.0-dev ttf-unifont ttf-dejavu ttf-dejavu-core ttf-dejavu-extra git build-essential python-nose libgdal1-dev python-gdal postgresql-9.3 postgresql-server-dev-9.3 postgresql-contrib-9.3 postgresql-9.3-postgis-2.1 libpq-dev php5 libsqlite3-dev
+apt-get install -y postgresql-9.1 postgresql-server-dev-9.1 postgresql-contrib-9.1 postgis postgresql-9.1-postgis libpq-dev php5 libsqlite3-dev
 
 # install mapnik packages
-apt-get install -y python-software-properties libmapnik-dev mapnik-utils python-mapnik
-apt-get install -y subversion git-core tar unzip wget bzip2 build-essential aptitude autoconf automake libtool make g++ libboost-all-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libxml2-dev libgeos-dev libgeos++-dev libpq-dev libbz2-dev libproj-dev protobuf-c-compiler libprotobuf-c0-dev lua5.2 liblua5.2-dev libboost-all-dev munin-node munin libfreetype6-dev libpng12-dev libtiff4-dev libicu-dev libgdal-dev libcairo-dev libcairomm-1.0-dev libagg-dev liblua5.2-dev ttf-unifont lua5.1 liblua5.1-dev node-carto
+
+apt-get purge libmapnik* mapnik-* python-mapnik # If exists
+
+apt-get install -y libicu-dev python-dev libxml2 libxml2-dev libfreetype6 libfreetype6-dev libjpeg-dev libpng-dev libproj-dev libtiff-dev libcairo2 libcairo2-dev python-cairo python-cairo-dev libcairomm-1.0-1 libcairomm-1.0-dev ttf-unifont ttf-dejavu ttf-dejavu-core ttf-dejavu-extra git build-essential python-nose libgdal1-dev python-gdal 
+apt-get install -y subversion git-core tar unzip wget bzip2 build-essential aptitude autoconf automake libtool make g++ libboost-all-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libxml2-dev libgeos-dev libgeos++-dev libpq-dev libbz2-dev libproj-dev protobuf-c-compiler libprotobuf-c0-dev lua5.2 liblua5.2-dev libboost-all-dev munin-node munin libfreetype6-dev libpng12-dev libtiff4-dev libicu-dev libgdal-dev libcairo-dev libcairomm-1.0-dev libagg-dev liblua5.2-dev ttf-unifont lua5.1 liblua5.1-dev 
+
+# node-carto
+
+#apt-get install -y libboost-filesystem1.48-dev libboost-program-options1.48-dev libboost-python1.48-dev libboost-regex1.48-dev libboost-system1.48-dev libboost-thread1.48-dev
+apt-get install -y python-software-properties
+add-apt-repository ppa:mapnik/boost
+apt-get install -y libmapnik-dev mapnik-utils python-mapnik
+apt-get install libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-python-dev libboost-regex-dev libboost-system-dev libboost-thread-dev
+apt-get install libboost-filesystem-dev libboost-program-options-dev libboost-python-dev libboost-regex-dev libboost-system-dev libboost-thread-dev
 
 # Install apache packages
 apt-get install -y apache2 apache2-dev apache2-mpm-worker
 
-# Install other packages
+# Install other required packages
 apt-get install -y curl
 apt-get install -y git
