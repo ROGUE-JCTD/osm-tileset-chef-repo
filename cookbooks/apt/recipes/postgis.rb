e6cca0bc0aa67d12e4df53dd057f9ece8604c417
Moore <s30245@1630557B-S30245.home>
2015-05-14 19:25:08 -0400

    modified:   Vagrantfile 	new file:   cookbooks/apt/recipes/postgis.rb

---
+++ cookbooks/apt/recipes/postgis.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: apt
+# Recipe:: default
+#
+# Copyright 2015, JAM
+#
+
+package "postgis"
+include_recipe postgresql_databaseo
+
+postgresql_database 'osm' do
+  connection(
+    :host      => '127.0.0.1',
+    :port      => 5432,
+    :username  => 'postgres',
+    :password  => node['postgresql']['password']['postgres']
+  )
+  action :create
+end
