d777b716ba85cc82f1d4fada0c73069e15257400
Moore <s30245@1630557B-S30245.home>
2015-05-15 10:45:49 -0400

    modified:   Berksfile 	modified:   scripts/install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -11,7 +11,7 @@ apt-get install -y postgresql-9.1 postgresql-server-dev-9.1 postgresql-contrib-9
 
 # install mapnik packages
 
-apt-get purge libmapnik* mapnik-* python-mapnik* # If exists
+apt-get purge -y libmapnik* mapnik-* python-mapnik* # If exists
 
 apt-get install -y libxml2 libxml2-dev libfreetype6 libfreetype6-dev libjpeg-dev libpng-dev libproj-dev libtiff-dev libcairo2 libcairo2-dev python-dev python-cairo python-cairo-dev libcairomm-1.0-1 libcairomm-1.0-dev ttf-unifont ttf-dejavu ttf-dejavu-core ttf-dejavu-extra git build-essential python-nose libgdal1-dev python-gdal 
 apt-get install -y subversion git-core tar unzip wget bzip2 build-essential aptitude autoconf automake libtool make g++ libboost-all-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libgeos-dev libgeos++-dev libpq-dev libbz2-dev protobuf-c-compiler libprotobuf-c0-dev lua5.2 liblua5.2-dev libboost-all-dev munin-node munin libpng12-dev libtiff4-dev libicu-dev libgdal-dev libcairo-dev libagg-dev liblua5.2-dev lua5.1 liblua5.1-dev 
