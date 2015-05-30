332d9c026636b128b4f34564d9b8de76b6575fe9
James Moore <jam@new-host.home>
2015-05-29 20:21:36 -0400

    new file:   cookbooks/postgresql/attributes/default.rb 	deleted:    cookbooks/postgresql/default.rb 	modified:   cookbooks/postgresql/recipes/db.rb

--- cookbooks/postgresql/recipes/db.rb
+++ cookbooks/postgresql/recipes/db.rb
@@ -9,5 +9,5 @@ package "postgresql-contrib"
 
 execute "create new postgresql database" do
    user "postgres"
-   command "psql -c \"create database #{node['osm']} owner #{node['db']['user']['name'];\""
+   command "psql -c \"create database #{node['db_name']};\""
 end
