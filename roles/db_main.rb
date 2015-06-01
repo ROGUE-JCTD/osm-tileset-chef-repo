e7739e135a9e0ef2bf28d039a1c54ec59894b994
Moore <s30245@1630557b-s30245.mitretek.org>
2015-06-01 13:19:17 -0400

    modified:   roles/db_main.rb

--- roles/db_main.rb
+++ roles/db_main.rb
@@ -1,10 +1,25 @@
-{
-  "name": "db_main",
-  "chef_type": "role",
-  "json_class": "Chef::Role",
-  "description": "Main Mapnik DB install package"
-  "run_list": [
-    "recipe[postgresql::packages]"
-    "recipe[postgresql::db]"
-  ]
-}
+name "db_main"
+description "Main Geoshape Mapnik PostGIS install package"
+
+default_attributes(
+  :postgresql => {
+    :versions => ["9.1"],
+    :settings => {
+      :defaults => {
+        :wal_level => "hot_standby",
+        :archive_mode => "on",
+        :archive_command => "/bin/cp %p /store/postgresql/archive/%f",
+        :max_wal_senders => "2",
+        :late_authentication_rules => [
+          { :database => "replication", :user => "replication", :address => "146.179.159.168/32" },
+          { :database => "replication", :user => "replication", :address => "146.179.159.170/32" }
+        ]
+      }
+    }
+  }
+)
+
+run_list[
+ "recipe[postgresql::packages]",
+ "recipe[postgresql::db]"
+]
