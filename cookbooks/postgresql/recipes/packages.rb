091858361c4d179c7c1e26668054b39a81b71abd
Moore <s30245@1630557b-s30245.mitretek.org>
2015-06-01 14:05:00 -0400

    modified:   cookbooks/postgresql/recipes/packages.rb

--- cookbooks/postgresql/recipes/packages.rb
+++ cookbooks/postgresql/recipes/packages.rb
@@ -1,5 +1,5 @@
 # install Postgresql packages.  These include packages necessary for postgis and mapnik.
 
-["postgresql-9.1", "postgresql-server-dev-9.1", "postgresql-contrib-9.1", "postgis postgresql-9.1-postgis", "libpq-dev", "php5", "libsqlite3-dev].each do |p|
+["postgresql-9.1", "postgresql-server-dev-9.1", "postgresql-contrib-9.1", "postgis postgresql-9.1-postgis", "libpq-dev", "php5", "libsqlite3-dev"].each do |p|
   package p
 end
