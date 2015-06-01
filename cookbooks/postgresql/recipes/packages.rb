3ab9b28b41ceff4c94111a957152531ef362153a
Moore <s30245@1630557b-s30245.mitretek.org>
2015-06-01 15:46:44 -0400

    modified:   cookbooks/postgresql/recipes/packages.rb

--- cookbooks/postgresql/recipes/packages.rb
+++ cookbooks/postgresql/recipes/packages.rb
@@ -1,5 +1,13 @@
 # install Postgresql packages.  These include packages necessary for postgis and mapnik.
 
+execute "add-apt-repository ppa:#{node[:ubuntugis/ppa]}" do
+  user "root"
+end
+
+execute "apt-get update" do
+  user "root"
+end
+
 ["postgresql-9.1", "postgresql-server-dev-9.1", "postgresql-contrib-9.1", "postgis postgresql-9.1-postgis", "libpq-dev", "php5", "libsqlite3-dev"].each do |p|
   package p
 end
