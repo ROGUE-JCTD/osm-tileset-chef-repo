0cce9153cbd7c9e51a6484d6efd04d694b1ebaa2
Moore <s30245@1630557b-s30245.mitretek.org>
2015-06-01 12:51:47 -0400

    modified:   cookbooks/postgresql/recipes/packages.rb 	modified:   roles/db_main.rb

--- roles/db_main.rb
+++ roles/db_main.rb
@@ -2,7 +2,7 @@
   "name": "db_main",
   "chef_type": "role",
   "json_class": "Chef::Role",
-  "description": "Main Mapnik DB install package",
+  "description": "Main Mapnik DB install package"
   "run_list": [
     "recipe[postgresql::packages]"
     "recipe[postgresql::db]"
