29f18a2828e4e8d70074d5ee425659537caa5c50
James Moore <jam@new-host-2.home>
2015-06-01 19:16:20 -0400

    modified:   cookbooks/postgresql/recipes/packages.rb

--- cookbooks/postgresql/recipes/packages.rb
+++ cookbooks/postgresql/recipes/packages.rb
@@ -8,6 +8,10 @@ execute "apt-get update" do
   user "root"
 end
 
-["postgresql-9.1", "postgresql-server-dev-9.1", "postgresql-contrib-9.1", "postgresql-9.1-postgis", "libpq-dev", "php5", "libsqlite3-dev"].each do |p|
+["postgresql-9.1", "postgresql-server-dev-9.1", "postgresql-contrib-9.1", "libpq-dev", "php5", "libsqlite3-dev"].each do |p|
+  package p
+end
+
+["postgis", "postgresql-9.1-postgis"].each do |p|
   package p
 end
