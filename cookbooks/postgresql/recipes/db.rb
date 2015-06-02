3f23f664d98ace2e8e2e962d3bbf95624dcdfa6a
Moore <s30245@1630557b-s30245.mitretek.org>
2015-06-02 14:14:08 -0400

    modified:   cookbooks/postgresql/attributes/default.rb 	modified:   cookbooks/postgresql/recipes/db.rb

--- cookbooks/postgresql/recipes/db.rb
+++ cookbooks/postgresql/recipes/db.rb
@@ -3,7 +3,7 @@ package "postgresql-contrib"
 
 execute "create role" do
    user "postgres"
-   command "psql -c \"create role #{node['db_name_admin']} with createdb inherit login encrypted password '#{node[\"db_admin_password\"]}';\""
+   command "psql -c \"create role #{node['db_name_admin']} with createdb inherit login encrypted password #{node['db_admin_password']}';\""
 end
 
 execute "create mapnik database" do
