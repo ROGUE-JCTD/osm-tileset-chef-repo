d3183ac29a12e28a815a6656fc0a6d744d003b69
James Moore <jam@new-host-2.home>
2015-06-02 18:02:21 -0400

    modified:   cookbooks/postgresql/recipes/db.rb

--- cookbooks/postgresql/recipes/db.rb
+++ cookbooks/postgresql/recipes/db.rb
@@ -3,7 +3,7 @@ package "postgresql-contrib"
 
 execute "create role" do
    user "postgres"
-   command "psql -c \"create role #{node['db_admin_name']} with createdb inherit;\""
+   command "psql -c \"create role #{node['db_admin_name']} with createdb inherit login;\""
 end
 
 execute "create mapnik database" do
