c96c1be65381071ade07a63c49cd91bf1b0bdb69
James Moore <jam@new-host-2.home>
2015-06-01 20:13:38 -0400

    modified:   cookbooks/postgresql/recipes/packages.rb

--- cookbooks/postgresql/recipes/packages.rb
+++ cookbooks/postgresql/recipes/packages.rb
@@ -1,29 +1,13 @@
 # install Postgresql packages.  These include packages necessary for postgis and mapnik.
 
-#execute "apt-add-repository -y ppa:ubuntugis/ppa" do
-#  user "root"
-#end
-
-execute "apt-add-repository -y ppa:sharpie/for-science" do
-  user "root"
-end
-
-execute "apt-add-repository -y ppa:sharpie/postgis-stable" do
-  user "root"
-end
-
-execute "apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable" do
-  user "root"
-end
-
 execute "apt-get update" do
   user "root"
 end
 
-["postgresql-9.1", "postgresql-server-dev-9.1", "postgresql-contrib-9.1", "libpq-dev", "php5", "libsqlite3-dev"].each do |p|
+["libc6", "libgdal1h", libgeos-c1", liblgeom-2.1.3", "libproj0", "libxml2"].each do |p|
   package p
 end
 
-["postgis", "postgresql-9.1-postgis2"].each do |p|
+["postgresql-9.4", "postgresql-9.4-postgis-scripts", "postgis", "postgresql-contrib-9.4", "libpq-dev", "php5", "libsqlite3-dev"].each do |p|
   package p
 end
