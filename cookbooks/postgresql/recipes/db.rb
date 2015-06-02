03a2e9b765f6e3ea57819110d50bf0f738a85ea8
Moore <s30245@1630557b-s30245.mitretek.org>
2015-06-02 13:44:58 -0400

    modified:   cookbooks/postgresql/attributes/default.rb 	modified:   cookbooks/postgresql/recipes/db.rb 	deleted:    data_bags/users/rogue.json 	deleted:    resources/account.rb

--- cookbooks/postgresql/recipes/db.rb
+++ cookbooks/postgresql/recipes/db.rb
@@ -1,7 +1,12 @@
 package "postgresql"
 package "postgresql-contrib"
 
-execute "create new postgresql database" do
+execute "create role" do
    user "postgres"
-   command "psql -c \"create database #{node['db_name']};\""
+   command "psql -c \"create role #{node['db_name_admin']} with createdb inherit login encrypted password '#{node[\'db_admin_password\']}';\""
+end
+
+execute "create mapnik database" do
+   user "postgres"
+   command "psql -c \"create database #{node['db_name']} owner #{node['db_name_admin']};\""
 end
