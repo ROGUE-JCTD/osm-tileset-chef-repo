ecf6b3133a6f1f27c989bdd8e05920c596a12af9
James Moore <jam@new-host.home>
2015-06-02 20:58:32 -0400

    modified:   cookbooks/countries/recipes/default.rb

--- cookbooks/countries/recipes/default.rb
+++ cookbooks/countries/recipes/default.rb
@@ -1,4 +1,4 @@
 execute 'wget_country_file' do
-  command "wget #{node[:countries][:name]} -O /opt/countries"
+  command "wget #{node[:countries][:name][:url]} -O /opt/countries"
   action :run
 end
