0bfcda6555583bdaab806c8c457169e23a3e564b
James Moore <jam@new-host.home>
2015-06-02 21:19:26 -0400

    modified:   roles/countries.rb

--- roles/countries.rb
+++ roles/countries.rb
@@ -5,9 +5,9 @@ default_attributes(
   :countries  => {
     :name => {
 #     :north_america => {:url => http://download.geofabrik.de/north-america-latest.osm.bz2 }'
-#     :central_america => {url => http://download.geofabrik.de/central-america-latest.osm.bz2 },
-     :guatemala => {url => "http://download.geofabrik.de/central-america/guatemala-latest.osm.bz2"},
-     :haiti_dominican_repub => {url => "http://download.geofabrik.de/central-america/haiti-and-domrep.html"}
+#     :central_america => {:url => http://download.geofabrik.de/central-america-latest.osm.bz2 },
+     :guatemala => {:url => "http://download.geofabrik.de/central-america/guatemala-latest.osm.bz2"},
+     :haiti_dominican_repub => {:url => "http://download.geofabrik.de/central-america/haiti-and-domrep.html"}
      }
    }
 )
