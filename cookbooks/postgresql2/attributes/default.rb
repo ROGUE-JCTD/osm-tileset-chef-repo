42627f2dc66d57c44de6f1415f61497d68a54d48
Moore <s30245@1630557b-s30245.mitretek.org>
2015-06-04 14:09:24 -0400

    new file:   cookbooks/.DS_Store 	new file:   cookbooks/database/CHANGELOG.md 	new file:   cookbooks/database/README.md 	new file:   cookbooks/database/libraries/matchers.rb 	new file:   cookbooks/database/libraries/provider_database_mysql.rb 	new file:   cookbooks/database/libraries/provider_database_mysql_user.rb 	new file:   cookbooks/database/libraries/provider_database_postgresql.rb 	new file:   cookbooks/database/libraries/provider_database_postgresql_schema.rb 	new file:   cookbooks/database/libraries/provider_database_postgresql_user.rb 	new file:   cookbooks/database/libraries/provider_database_sql_server.rb 	new file:   cookbooks/database/libraries/provider_database_sql_server_user.rb 	new file:   cookbooks/database/libraries/resource_database.rb 	new file:   cookbooks/database/libraries/resource_database_user.rb 	new file:   cookbooks/database/libraries/resource_mysql_database.rb 	new file:   cookbooks/database/libraries/resource_mysql_database_user.rb 	new file:   cookbooks/database/libraries/resource_postgresql_database.rb 	new file:   cookbooks/database/libraries/resource_postgresql_database_schema.rb 	new file:   cookbooks/database/libraries/resource_postgresql_database_user.rb 	new file:   cookbooks/database/libraries/resource_sql_server_database.rb 	new file:   cookbooks/database/libraries/resource_sql_server_database_user.rb 	new file:   cookbooks/database/metadata.json 	new file:   cookbooks/database/recipes/postgresql.rb 	new file:   cookbooks/database/templates/default/app_grants.sql.erb 	new file:   cookbooks/database/templates/default/aws_config.erb 	new file:   cookbooks/database/templates/default/chef-solo-database-snapshot.cron.erb 	new file:   cookbooks/database/templates/default/chef-solo-database-snapshot.json.erb 	new file:   cookbooks/database/templates/default/chef-solo-database-snapshot.rb.erb 	new file:   cookbooks/database/templates/default/ebs-backup-cron.erb 	new file:   cookbooks/database/templates/default/ebs-db-backup.sh.erb 	new file:   cookbooks/database/templates/default/ebs-db-restore.sh.erb 	new file:   cookbooks/database/templates/default/s3cfg.erb 	new file:   cookbooks/postgresql/CHANGELOG.md 	new file:   cookbooks/postgresql/README.md 	new file:   cookbooks/postgresql/files/default/tests/minitest/apt_pgdg_postgresql_test.rb 	new file:   cookbooks/postgresql/files/default/tests/minitest/default_test.rb 	new file:   cookbooks/postgresql/files/default/tests/minitest/ruby_test.rb 	new file:   cookbooks/postgresql/files/default/tests/minitest/server_test.rb 	new file:   cookbooks/postgresql/files/default/tests/minitest/support/helpers.rb 	new file:   cookbooks/postgresql/libraries/default.rb 	new file:   cookbooks/postgresql/metadata.json 	new file:   cookbooks/postgresql/metadata.rb 	new file:   cookbooks/postgresql/recipes/apt_pgdg_postgresql.rb 	new file:   cookbooks/postgresql/recipes/client.rb 	new file:   cookbooks/postgresql/recipes/config_initdb.rb 	new file:   cookbooks/postgresql/recipes/config_pgtune.rb 	new file:   cookbooks/postgresql/recipes/contrib.rb 	new file:   cookbooks/postgresql/recipes/ruby.rb 	new file:   cookbooks/postgresql/recipes/server.rb 	new file:   cookbooks/postgresql/recipes/server_conf.rb 	new file:   cookbooks/postgresql/recipes/server_debian.rb 	new file:   cookbooks/postgresql/recipes/server_redhat.rb 	new file:   cookbooks/postgresql/recipes/yum_pgdg_postgresql.rb 	new file:   cookbooks/postgresql/templates/default/pg_hba.conf.erb 	new file:   cookbooks/postgresql/templates/default/pgsql.sysconfig.erb 	new file:   cookbooks/postgresql/templates/default/postgresql.conf.erb 	new file:   cookbooks/postgresql2/attributes/default.rb 	new file:   cookbooks/postgresql2/recipes/db.rb 	new file:   cookbooks/postgresql2/recipes/default.rb 	new file:   cookbooks/postgresql2/recipes/packages.rb 	new file:   cookbooks/postgresql2/recipes/postgis.rb 	deleted:    metadata.rb

---
+++ cookbooks/postgresql2/attributes/default.rb
@@ -0,0 +1,5 @@
+default['db_name'] = 'osm'
+default['db_admin_name'] = 'osmdata'
+default['db_render_name'] = 'osmrender'
+default['db_password'] = '$1$ngB3EurO$i9SlxPxJoSUPhcLkYqcFm0'
+default['db_admin_password'] = '\'$1$HdgDew2V$KwNn98bP9tHgUpbsNSGAR0\''
--- metadata.rb
+++
@@ -1,6 +0,0 @@
-%w(postgresql).each do |cb|
-  depends cb
-end
-
-depends 'postgresql'
-depends 'database'
