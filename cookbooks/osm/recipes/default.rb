013017c829c0be92107dbbc97ae6cecc60d101db
James Moore <jam@new-host.home>
2015-06-03 21:01:19 -0400

    modified:   cookbooks/osm/recipes/default.rb

--- cookbooks/osm/recipes/default.rb
+++ cookbooks/osm/recipes/default.rb
@@ -1,3 +1,6 @@
+execute 'mkdir /home/osmdata/data' do
+   user 'osmdata'
+end
 execute 'git clone https://github.com/gravitystorm/openstreetmap-carto.git' do
    cwd '/home/osmdata/data'
    user 'osmdata'
