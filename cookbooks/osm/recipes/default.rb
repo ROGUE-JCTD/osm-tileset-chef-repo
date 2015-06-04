1fa54f53143e30a69631516f575551ef4c39b78e
Moore <s30245@1630557b-s30245.mitretek.org>
2015-06-04 11:11:28 -0400

    modified:   cookbooks/osm/recipes/default.rb

--- cookbooks/osm/recipes/default.rb
+++ cookbooks/osm/recipes/default.rb
@@ -1,11 +1,23 @@
 execute 'mkdir /home/osmdata/src' do
    user 'osmdata'
 end
-#execute 'mkdir /home/osmdata/src/carto' do
-#   user 'osmdata'
-#end
 execute 'git clone https://github.com/gravitystorm/openstreetmap-carto.git' do
    cwd '/home/osmdata/src'
    user 'osmdata'
 end
-
+execute 'git pull && git checkout' do
+   cwd '/home/osmdata/src/openstreetmap-carto'
+   user 'osmdata'
+end
+execute 'git clone https://github.com/mapbox/carto.git' do
+   cwd '/home/osmdata/src'
+   user 'osmdata'
+end
+execute 'git pull && git checkout' do
+   cwd '/home/osmdata/src/carto'
+   user 'osmdata'
+end
+execute 'npm install' do
+   user 'osmdata'
+   cwd '/home/osmdata/src/carto'
+end
