9a34b9087dafe5a0f812827d7b59e4bdeab474d0
James Moore <jam@new-host-3.home>
2015-06-11 08:20:38 -0400

    modified:   cookbooks/geoshapeosm/recipes/osm.rb

--- cookbooks/geoshapeosm/recipes/osm.rb
+++ cookbooks/geoshapeosm/recipes/osm.rb
@@ -1,33 +1,77 @@
-execute 'mkdir /home/osmdata/src' do
+bash 'configure_carto_tree' do
    user 'osmdata'
+   cwd '/home/osmdata'
+   code <<-EOH
+	cd /home/osmdata
+	mkdir /home/osmdata/src
+	git clone https://github.com/gravitystorm/openstreetmap-carto.git
+	cd /home/osmdata/src/carto
+	git pull && git checkout
+   EOH
 end
-execute 'git clone https://github.com/gravitystorm/openstreetmap-carto.git' do
-   cwd '/home/osmdata/src'
-   user 'osmdata'
-end
-execute 'git pull && git checkout' do
-   cwd '/home/osmdata/src/openstreetmap-carto'
-   user 'osmdata'
-end
-execute 'git clone https://github.com/mapbox/carto.git' do
-   cwd '/home/osmdata/src'
-   user 'osmdata'
-end
-execute 'git pull && git checkout' do
-   cwd '/home/osmdata/src/carto'
-   user 'osmdata'
-end
+
 execute 'add-apt-repository -y ppa:chris-lea/node.js' do
-   cwd '/home/osmdata/src'
 end
 execute 'apt-get update' do
 end
-execute 'apt-get install -y nodejs' do
+
+
+bash 'install_nodejs' do
+   user 'root'
    cwd '/home/osmdata/src'
+   code <<-EOH
+	apt-get install -y nodejs
+   EOH
 end
-execute 'cd /home/osmdata/src;npm install npm' do
+
+bash 'configure_carto_tree' do
    user 'osmdata'
+   cwd '/home/osmdata/src'
+   code <<-EOH
+	cd /home/osmdata/src
+	npm install npm
+   EOH
 end
-execute 'npm install millstone carto' do
+
+bash 'configure_carto_tree' do
    user 'osmdata'
+   cwd '/home/osmdata/src'
+   code <<-EOH
+	cd /home/osmdata/src
+	npm install millstone carto
+   EOH
 end
+
+#execute 'mkdir /home/osmdata/src' do
+#   user 'osmdata'
+#end
+#execute 'git clone https://github.com/gravitystorm/openstreetmap-carto.git' do
+#   cwd '/home/osmdata/src'
+#   user 'osmdata'
+#end
+#execute 'git pull && git checkout' do
+#   cwd '/home/osmdata/src/openstreetmap-carto'
+#   user 'osmdata'
+#end
+#execute 'git clone https://github.com/mapbox/carto.git' do
+#   cwd '/home/osmdata/src'
+#   user 'osmdata'
+#end
+#execute 'git pull && git checkout' do
+#   cwd '/home/osmdata/src/carto'
+#   user 'osmdata'
+#end
+#execute 'add-apt-repository -y ppa:chris-lea/node.js' do
+#   cwd '/home/osmdata/src'
+#end
+#execute 'apt-get update' do
+#end
+#execute 'apt-get install -y nodejs' do
+#   cwd '/home/osmdata/src'
+#end
+#execute 'cd /home/osmdata/src;npm install npm' do
+#   user 'osmdata'
+#end
+#execute 'npm install millstone carto' do
+#   user 'osmdata'
+#end
