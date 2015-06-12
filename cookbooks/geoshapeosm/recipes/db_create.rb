2136a831ae3e9a63bb29878422a8c2e8ee121226
James Moore <jam@new-host-2.home>
2015-06-12 11:21:55 -0400

    modified:   cookbooks/geoshapeosm/recipes/db_create.rb 	modified:   cookbooks/geoshapeosm/recipes/users.rb

--- cookbooks/geoshapeosm/recipes/db_create.rb
+++ cookbooks/geoshapeosm/recipes/db_create.rb
@@ -4,11 +4,13 @@ package "postgresql-contrib"
 execute "create role" do
    user "postgres"
    command "psql -c \"create role #{node['db_admin_name']} with createdb inherit login;\""
+   not_if "psql -c "SELECT rolname FROM pg_roles where rolname = '#{node['db_admin_name']}'";
 end
 
 execute "create role" do
    user "postgres"
    command "psql -c \"create role #{node['db_render_name']} with inherit login;\""
+   not_if "psql -c "SELECT rolname FROM pg_roles where rolname = '#{node['db_render_name']}'";
 end
 
 execute "create osm database" do
