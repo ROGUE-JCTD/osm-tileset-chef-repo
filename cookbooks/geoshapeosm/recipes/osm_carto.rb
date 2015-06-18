63670424265a3a9cd66ff6e8df30f953cd7c567b
Moore <s30245@1630557b-s30245.mitretek.org>
2015-06-18 14:00:32 -0400

    modified:   ../../../.gitignore 	modified:   osm_carto.rb

--- cookbooks/geoshapeosm/recipes/osm_carto.rb
+++ cookbooks/geoshapeosm/recipes/osm_carto.rb
@@ -52,42 +52,8 @@ end
 execute 'chown -R osmdata:osmdata /home/osmdata/src' do
    user 'root'
 end
-
-execute './get-shapefiles.sh' do
-   cwd '/home/osmdata/src/openstreetmap-carto'
-   user 'osmdata'
-end
-
-#execute 'mkdir /home/osmdata/src' do
-#   user 'osmdata'
-#end
-#execute 'git clone https://github.com/gravitystorm/openstreetmap-carto.git' do
-#   cwd '/home/osmdata/src'
-#   user 'osmdata'
-#end
-#execute 'git pull && git checkout' do
+#
+#execute './get-shapefiles.sh' do
 #   cwd '/home/osmdata/src/openstreetmap-carto'
-#   user 'osmdata'
-#end
-#execute 'git clone https://github.com/mapbox/carto.git' do
-#   cwd '/home/osmdata/src'
-#   user 'osmdata'
-#end
-#execute 'git pull && git checkout' do
-#   cwd '/home/osmdata/src/carto'
-#   user 'osmdata'
-#end
-#execute 'add-apt-repository -y ppa:chris-lea/node.js' do
-#   cwd '/home/osmdata/src'
-#end
-#execute 'apt-get update' do
-#end
-#execute 'apt-get install -y nodejs' do
-#   cwd '/home/osmdata/src'
-#end
-#execute 'cd /home/osmdata/src;npm install npm' do
-#   user 'osmdata'
-#end
-#execute 'npm install millstone carto' do
 #   user 'osmdata'
 #end
