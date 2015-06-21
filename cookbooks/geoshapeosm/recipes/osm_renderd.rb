3ed8a18c9553811965999313eca09a20ac190194
James Moore <jam@new-host-2.home>
2015-06-21 09:56:05 -0400

    modified:   cookbooks/geoshapeosm/recipes/osm_renderd.rb

--- cookbooks/geoshapeosm/recipes/osm_renderd.rb
+++ cookbooks/geoshapeosm/recipes/osm_renderd.rb
@@ -1,6 +1,6 @@
 bash 'install_mod_tile' do
    user 'osmdata'
-   cwd '/home/osmdata/src/mod_tile'
+   cwd '/home/osmdata/src'
    code <<-EOH
 	cd /home/osmdata/src
 	git clone git://github.com/openstreetmap/mod_tile.git
