161d3716674ad6adee70466584f248e286191a2c
James Moore <jam@new-host-2.home>
2015-06-21 09:05:54 -0400

    Revert "	modified:   cookbooks/geoshapeosm/recipes/osm_renderd.rb"
    This reverts commit 9f547147d96dce13d497b7ce360d36c234f29dba.
    
    	modified:   cookbooks/geoshapeosm/recipes/osm_renderd.rb

--- cookbooks/geoshapeosm/recipes/osm_renderd.rb
+++ cookbooks/geoshapeosm/recipes/osm_renderd.rb
@@ -1,6 +1,6 @@
 bash 'install_mod_tile' do
    user 'osmdata'
-   cwd '/home/osmdata/src'
+   cwd '/home/osmdata/src/mod_tile'
    code <<-EOH
 	cd /home/osmdata/src
 	git clone git://github.com/openstreetmap/mod_tile.git
