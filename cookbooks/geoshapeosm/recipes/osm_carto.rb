939a25c2f5977be834d70eb54b5bdcdebbe694f6
James Moore <jam@Jamess-Mac-mini.home>
2015-06-22 18:39:54 -0400

    modified:   cookbooks/geoshapeosm/recipes/apt-updates.rb 	modified:   cookbooks/geoshapeosm/recipes/osm_carto.rb

--- cookbooks/geoshapeosm/recipes/osm_carto.rb
+++ cookbooks/geoshapeosm/recipes/osm_carto.rb
@@ -16,39 +16,6 @@ bash 'configure_carto_tree' do
    EOH
 end
 
-execute 'add-apt-repository -y ppa:chris-lea/node.js' do
-end
-
-execute 'apt-get update' do
-end
-
-bash 'install_nodejs' do
-   cwd '/home/osmdata/src'
-   code <<-EOH
-	apt-get install -y nodejs
-   EOH
-end
-
-bash 'install_npm' do
-   user 'root'
-   cwd '/home/osmdata/src'
-   code <<-EOH
-	su - osmdata
-	cd /home/osmdata/src
-	npm install npm
-   EOH
-end
-
-bash 'install_millstone' do
-   user 'root'
-   cwd '/home/osmdata/src'
-   code <<-EOH
-	su - osmdata
-	cd /home/osmdata/src
-	npm install millstone carto
-   EOH
-end
-
 execute './get-shapefiles.sh' do
    cwd '/home/osmdata/src/openstreetmap-carto'
    user 'osmdata'
@@ -57,3 +24,45 @@ end
 execute 'chown -R osmdata:osmdata /home/osmdata/src' do
    user 'root'
 end
+
+##execute 'add-apt-repository -y ppa:chris-lea/node.js' do
+##end
+##
+##execute 'apt-get update' do
+##end
+##
+##bash 'install_nodejs' do
+##   cwd '/home/osmdata/src'
+##   code <<-EOH
+##	apt-get install -y nodejs
+##   EOH
+##end
+##
+##bash 'install_npm' do
+##   user 'root'
+##   cwd '/home/osmdata/src'
+##   code <<-EOH
+##	su - osmdata
+##	cd /home/osmdata/src
+##	npm install npm
+##   EOH
+##end
+##
+##bash 'install_millstone' do
+##   user 'root'
+##   cwd '/home/osmdata/src'
+##   code <<-EOH
+##	su - osmdata
+##	cd /home/osmdata/src
+##	npm install millstone carto
+##   EOH
+##end
+##
+##execute './get-shapefiles.sh' do
+##   cwd '/home/osmdata/src/openstreetmap-carto'
+##   user 'osmdata'
+##end
+##
+##execute 'chown -R osmdata:osmdata /home/osmdata/src' do
+##   user 'root'
+##end
