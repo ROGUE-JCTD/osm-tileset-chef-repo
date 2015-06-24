a50e0d0226aa427fcf24dc9da572353c2c8797de
James Moore <jam@Jamess-Mac-mini.home>
2015-06-24 07:39:15 -0400

    modified:   cookbooks/geoshapeosm/recipes/osm_carto.rb 	modified:   cookbooks/geoshapeosm/recipes/osm_renderd.rb

--- cookbooks/geoshapeosm/recipes/osm_renderd.rb
+++ cookbooks/geoshapeosm/recipes/osm_renderd.rb
@@ -33,8 +33,15 @@ end
 #   EOH
 #end
 
-execute "sed -i 's/XML=\\/home.\\/jburgess\\/osm\/svn\\.openstreetmap\\.org\\/applications\\/rendering\\/mapnik\\/osm\\-local\\.xml/XML=\\/home\\/osmdata\\/src\\/openstreetmap-carto\\/mapnik.xml/' /usr/local/etc/renderd.conf" do
+execute "sed -i 's/XML=\\/home\\/jburgess\\/osm\\/svn\.openstreetmap\.org\\/applications\\/rendering\\/mapnik\\/osm\-local\.xml/XML=\\/home\\/osm\\/openstreetmap-carto\\/style.xml/' /usr/local/etc/renderd.conf" do
 end
+
+execute "sed -i 's/HOST=tile\.openstreetmap\.org/HOST=localhost/' /usr/local/etc/renderd.conf" do
+end
+
+execute "sed -i 's/plugins_dir=\\/usr\\/lib\\/mapnik\\/input/plugins_dir=\\/usr\\/lib\\/mapnik\\/2.2\\/input\\//' /usr/local/etc/renderd.conf" do
+end
+
 
 execute "LoadTileConfigFile /usr/local/etc/renderd.conf" do
 end
