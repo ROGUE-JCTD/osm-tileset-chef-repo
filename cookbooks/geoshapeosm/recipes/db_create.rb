6aa17ed297dfe24ddce7059cf275920c4b5be937
James Moore <jnltech2@gmail.com>
2015-07-27 12:43:11 -0400

    Update db_create.rb
    Added hstore check

--- cookbooks/geoshapeosm/recipes/db_create.rb
+++ cookbooks/geoshapeosm/recipes/db_create.rb
@@ -1,5 +1,3 @@
-#   not_if "psql -c \"SELECT rolname FROM pg_roles where rolname = '#{node['db_admin_name']}'\" | grep -c #{node['db_admin_name']}";
-
 execute "create role" do
    user "postgres"
    command "psql -c \"create role #{node['db_admin_name']} with createdb inherit login;\""
@@ -16,6 +14,12 @@ execute "create adminpack extension" do
    user "postgres"
    command "psql -d #{node['db_name']} -c \"create extension adminpack;\""
    not_if { `sudo -u postgres psql -d '#{node['db_name']}' -tAc \"SELECT * FROM pg_extension where extname = 'adminpack';\" | wc -l`.chomp == "1" }
+end
+
+execute "create hstore extension" do
+   user "postgres"
+   command "psql -d #{node['db_name']} -c \"create extension hstore;\""
+   not_if { `sudo -u postgres psql -d '#{node['db_name']}' -tAc \"SELECT * FROM pg_extension where extname = 'hstore';\" | wc -l`.chomp == "1" }
 end
 
 execute "munin-node-configure --sh | sudo sh" do
