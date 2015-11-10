1e2fba05853bc370ec2d81a30777e51df0b30336
Syrus Mesdaghi <syrusm@gmail.com>
2015-11-09 19:29:10 -0500

    Update install_datafile.sh
    removed exiting cached tiles to make sure updated data will result in the tiles being rendered again.

--- scripts/install_datafile.sh
+++ scripts/install_datafile.sh
@@ -2,6 +2,15 @@
 # exit if anything returns failure
 set -e
 
+while true; do
+    read -p "=> you must be running as the osmdata user for this task to succeed. Also note all data in current database will be dropped and then data will be imported from the temp folder. continue? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "    Aborting script!";exit 1;;
+        * ) echo "    Please answer yes or no.";;
+    esac
+done
+
 count=`ls -1 /tmp/*.pbf 2>/dev/null | wc -l`
   if [ $count != 0 ]
   then
@@ -19,3 +28,7 @@ count=`ls -1 /tmp/*.bz2 2>/dev/null | wc -l`
        osm2pgsql --create --slim -C 1500 --number-processes 1 -S /home/osmdata/src/openstreetmap-carto/openstreetmap-carto.style --hstore --multi-geometry $f
       done
    fi
+   
+service renderd stop
+rm -rf /var/lib/mod_tile/default/*
+service renderd start
